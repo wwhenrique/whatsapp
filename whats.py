b1eb9df1c8d4be825eeb42454c5b0bdf032529cf
Chris <wesley_fit@hotmail.com>
2022-08-02 12:53:23 -0300

    Adicionado arquivo.txt adicionado envio de mensagem randomica

--- whats.py
+++ whats.py
@@ -1,13 +1,13 @@
-from urllib.request import urlopen
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from time import sleep
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
+from time import sleep
 import io
+from random import randint
 
 class Whatsapp:
     def __init__(self):
@@ -57,15 +57,26 @@ class Whatsapp:
         linhas_arquivo = self.ler_arquivo()
 
 
+
         count = 0
         while True:
-            for linha in linhas_arquivo:
-                count+=1
-                mensagem.send_keys(linha)
-                mensagem.send_keys(Keys.ENTER)
-                print(f'Mensagem enviada {count}')
-                # define tempo de espera para enviar mensagem
-                sleep(10)
+            # envia em ordem
+            # for linha in linhas_arquivo:
+            #     count+=1
+            #     mensagem.send_keys(linha)
+            #     mensagem.send_keys(Keys.ENTER)
+            #     print(f'Mensagem enviada {count}')
+            #     # define tempo de espera para enviar mensagem
+            #     sleep(1)
+
+            #envia de maneira aleatoria
+            count += 1
+            mensagem.send_keys(linhas_arquivo[randint(0, len(linhas_arquivo))])
+            mensagem.send_keys(Keys.ENTER)
+            print(f'Mensagem enviada {count}')
+            # define tempo de espera para enviar mensagem
+            sleep(1)
+
 
 whats = Whatsapp()
 whats.iniciar()
